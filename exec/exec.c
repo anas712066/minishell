a0b33051f04f0a65414b9705fe60405b2bb39a8e
Matei-Stefan <mateimilitaru2001@gmail.com>
2025-06-29 15:12:59 +0200

    casi arreglado

--- exec/exec.c
+++ exec/exec.c
@@ -6,18 +6,9 @@
 /*   By: mmilitar <mmilitar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/02 16:54:42 by mumajeed          #+#    #+#             */
-/*   Updated: 2025/06/29 15:00:18 by mmilitar         ###   ########.fr       */
+/*   Updated: 2025/06/29 15:11:21 by mmilitar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <fcntl.h>      // Para open(), O_RDONLY, O_WRONLY, O_CREAT, etc.
-#include <string.h> 
-#include <sys/wait.h>
-#include <sys/stat.h>
 
 #include "../include/exec.h"
 
@@ -62,36 +53,33 @@ static int	execute_child_process(t_command *cmd,
 	return (0);
 }
 
-static char	*resolve_binary_path(t_command *cmd)
+static int	resolve_binary_path(t_command *cmd, char **binary_path)
 {
-	char	*binary_path;
-	int		ret;
+	int	ret;
 
+	*binary_path = NULL;
 	if (!cmd || !cmd->args || !cmd->args[0])
-		return (NULL);
+		return (1);
 	if (handle_empty_command(cmd))
-		return (NULL);
+		return (0);
 	if (cmd->args[0][0] == '.' || cmd->args[0][0] == '/')
 	{
 		ret = check_file_permissions(cmd->args[0]);
 		if (ret != 0)
-			return (NULL);
-		binary_path = ft_strdup(cmd->args[0]);
+			return (ret);
+		*binary_path = ft_strdup(cmd->args[0]);
+		return (0);
 	}
 	else
 	{
-		binary_path = find_binary_in_path(cmd->args[0]);
-		if (!binary_path)
-		{
-			handle_local_file(cmd->args[0], &binary_path);
-			return (NULL);
-		}
+		*binary_path = find_binary_in_path(cmd->args[0]);
+		if (!*binary_path)
+			return (handle_local_file(cmd->args[0], binary_path));
 	}
-	return (binary_path);
+	return (0);
 }
 
-static int	execute_fork_process(t_command *cmd,
-	char *binary_path, char ***envp)
+static int	execute_fork_process(t_command *cmd, char *binary_path, char ***envp)
 {
 	pid_t	pid;
 	int		status;
@@ -113,14 +101,17 @@ static int	execute_fork_process(t_command *cmd,
 		free(binary_path);
 		return (1);
 	}
-	return (0);
+	return (1);
 }
 
 int	execute_external_command(t_command *cmd, char ***envp)
 {
 	char	*binary_path;
+	int		resolve_result;
 
-	binary_path = resolve_binary_path(cmd);
+	resolve_result = resolve_binary_path(cmd, &binary_path);
+	if (resolve_result != 0)
+		return (resolve_result);
 	if (!binary_path)
 		return (1);
 	return (execute_fork_process(cmd, binary_path, envp));
